eac2a1092435b2dfaaee0b0337222f334cdf39ff
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-12 01:08:39 +0530

    Add files via upload

---
+++ Program to implement formatted Input Output function.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ip_char;
+    int ip_int;
+    float ip_float;
+
+    printf("\nEnter the char : ");
+    scanf("%c",&ip_char);
+
+    printf("\nEnter the int : ");
+    scanf("%d",&ip_int);
+
+    printf("\nEnter the float : ");
+    scanf("%f",&ip_float);
+
+    printf("\nOUTPUT:");
+
+    printf("\nThe given char  : %c",ip_char);
+    printf("\nThe given int   : %d",ip_int);
+    printf("\nThe given float : %ef",ip_float);
+
+    return 0;
+
+}
