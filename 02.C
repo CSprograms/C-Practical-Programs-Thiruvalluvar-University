8163a4d9d754d18439728db37f8087e5b901a6ac
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-15 07:11:43 +0530

    Add files via upload

---
+++ 02.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int input;
+
+    printf("\nEnter the input : ");
+    scanf("%d",&input);
+
+    if ( input % 2 == 0 )
+        printf("\nThe given input is even.\n");
+
+    if ( input % 2 )
+        printf("\nThe given input is odd.\n");
+
+    if ( input % 2 )
+        printf("\nThe given input is odd.\n");
+    else
+        printf("\nThe given input is even.\n");
+
+    return 0;
+}
